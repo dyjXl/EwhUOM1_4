0ff2075d30808da694f9751ce0005f1f5cbc465a
dyjXl <ujutjh36@gmail.com>
2022-09-28 11:09:34 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     let nfts; 
     console.log("fetching nfts");
     
-    const api_key = ""
+    const api_key = "pF3DClpltoBjXuLdkiK8MQagvGTipZdH"
 
 
 
@@ -44,7 +44,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = ""
+      const api_key = "pF3DClpltoBjXuLdkiK8MQagvGTipZdH"
 
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
